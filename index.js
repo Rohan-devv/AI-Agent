4aa937c3cea57d23b12ce59d66199b743804abe6
Rohan-devv <devv112004@gmail.com>
2025-09-29 23:34:59 +0530

    yes Gemini is workign

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
+const GEMINI_API_KEY = "AIzaSyB7Uco4d8evmh302DouJHC9OW0p4OG5TVI";
 
-const genAI = new GoogleGenerativeAI("AIzaSyB7Uco4d8evmh302DouJHC9OW0p4OG5TVI");
+const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
+// // client banaya hai yaha humne jisse hum baat kar sakte hai
 
-async function run() {
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+// Tools-->
 
-  const result = await model.generateContent("Say only: Hello Gemini!");
-  console.log(result.response.text());
+function getweather(city) {
+    if (city === "delhi") return "30 degree celsius";
+    if (city === "mumbai") return "35 degree celsius";
+    if (city === "kolkata") return "32 degree celsius";
 }
 
-run().catch(console.error);
+const user = "what is the weather of delhi";
+const city = "delhi";
+const weather = getweather(city);
+
+const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash-001' });
+
+
+
+model.generateContent(`User asked: ${user}. Real data: ${weather}.`)
+.then((result) => {
+    console.log(result.response.text());
+});
